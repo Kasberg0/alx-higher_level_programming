acd7e0e6e57048ee7f62612f5f8d0ff9e7d572a3
kasberg0 <regiskouassi242@gmail.com>
2023-02-20 10:07:46 -0800

    Mandatory Task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add as add
-
 if __name__ == "__main__":
+    from add_0 import add
+
     a = 1
     b = 2
-    print("{a} + {b} = {add}".format(a=a, b=b, add=add(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from sys import argv as argv
 
 if __name__ == "__main__":
-    somme = 0
-    for arg in range(1, len(argv)):
-        somme += int(argv[arg])
-    print(f"{somme}")
+    """Print the addition of all arguments."""
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
